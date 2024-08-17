a8cc9eee319c9e149971b4de25a8bd8c7949d94d
sayantan1821 <ksayantan1823@gmail.com>
2024-08-18 02:42:49 +0530

    fix dependencies add roles in jwt token

--- src/main/java/com/sayantan/userservice/security/models/CustomGrantedAuthority.java
+++ src/main/java/com/sayantan/userservice/security/models/CustomGrantedAuthority.java
@@ -2,12 +2,14 @@ package com.sayantan.userservice.security.models;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.sayantan.userservice.models.Role;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
 @JsonDeserialize
+@NoArgsConstructor(force = true)
 public class CustomGrantedAuthority implements GrantedAuthority {
 
-    private final String authority;
+    private String authority;
     public CustomGrantedAuthority(Role role) {
         this.authority = role.getValue();
     }
